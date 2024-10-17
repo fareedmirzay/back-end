858fb40eb9892c9145bfcf405d0eb913c5c472de
fareedmirzay <fareedmirzay@ymail.com>
2024-10-17 09:42:54 -0400

    express-lab-complete3

---
+++ models/Project.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import { taskSchema } from "./Task.js";
+
+const projectSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+    },
+    endDate: {
+      type: Date,
+    },
+    tasks: [],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tasks: [taskSchema],
+  },
+  { timestamps: true },
+);
+
+// index
+// pre save middlewares
+// methods
+
+
+const Project = new mongoose.model('Project', projectSchema);
+export default Project;
